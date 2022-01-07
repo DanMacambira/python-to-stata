147d7efdd06b0e83600e83a8abccafd46549ae7b
Danil Agafiev Macambira <67755817+DanMacambira@users.noreply.github.com>
2022-01-07 09:44:43 -0500

    Added new method to pass variable labels
    You can now pass variable labels in a dictionary such that they appear in the .dta file.
    
    Also made the force_nums method more robust

--- pandas_to_dta.py
+++ pandas_to_dta.py
@@ -1,7 +1,6 @@
 import os
 import subprocess
 
-
 def to_dta(stata_path, dataframe=None, output_path=None, file_name=None, force_nums=None, var_labels=None, log_path=None):
     """
     Export dataframe to Stata silently in the background
@@ -36,24 +35,20 @@ def to_dta(stata_path, dataframe=None, output_path=None, file_name=None, force_n
     # Stata locals
     params = [csv, dta]
 
-    # Number of columns to convert
-    if force_nums is None:
-        force_nums = []
-    else:
+    # Convert string columns to num type columns
+    if force_nums is not None:
         if isinstance(force_nums, list):
             pass
         else:
             force_nums = [force_nums]
-
-    num_columns = len(force_nums)
-
-    # Data types
-    if force_nums is not None:
+        force_nums = [var for var in force_nums if var in dataframe.columns]
+        num_columns = len(force_nums)
         params.extend(str(num_columns))
         params.extend(force_nums)
 
     # Variable Labels
     if var_labels is not None:
+        var_labels = {var: var_labels[var] for var in var_labels.keys() if var in dataframe.columns}
         labels = [(f'label var {var}', f'{var_labels[var]}') for var in var_labels.keys()]
         labels = list(sum(labels, ())) 
         num_columns = len(labels)
@@ -62,6 +57,7 @@ def to_dta(stata_path, dataframe=None, output_path=None, file_name=None, force_n
 
 
     #### RUN
+    print(params)
 
     # Stata Do File path
     dir_path = os.path.dirname(os.path.realpath(__file__))
