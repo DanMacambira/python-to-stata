fce79bfc2114979db47d5205e86964e91decbf52
Danil Agafiev Macambira <67755817+DanMacambira@users.noreply.github.com>
2022-01-07 08:48:12 -0500

    Added new method to pass variable labels
    You can now pass variable labels in a dictionary such that they appear in the .dta file

--- pandas_to_dta.py
+++ pandas_to_dta.py
@@ -2,15 +2,13 @@ import os
 import subprocess
 
 
-def to_dta(stata_path, dataframe=None, output_path=None, file_name=None, force_nums=None, log_path=None):
+def to_dta(stata_path, dataframe=None, output_path=None, file_name=None, force_nums=None, var_labels=None, log_path=None):
     """
     Export dataframe to Stata silently in the background
     Parameters
     ----------
     stata_path : str
         Full path to the Stata executable on your system, e.g., "C:\Program Files (x86)\Stata14\StataMP-64"
-    do_file : str
-        Full path to the Stata do file.
     dataframe :
         Dataframe to pass onto Stata.
     output_path : str
@@ -20,15 +18,10 @@ def to_dta(stata_path, dataframe=None, output_path=None, file_name=None, force_n
     force_nums: list of str
         Columns to be converted into num types with Stata's real() command.
         Note, this do file sets all dtypes to be strings in order to protect data types.
+    var_labels: dict of str, e.g. {'myvar1': 'mylabel1', 'myvar2': 'mylabel2'}
+        Labels to add to variables in Stata
     log_path : str
         Full path to save the log files. By default will save in the current working directory
-        :param log_path:
-        :param force_nums:
-        :param file_name:
-        :param output_path:
-        :param dataframe:
-        :param do_file:
-        :param stata_path:
     """
 
     #### PREP
@@ -39,6 +32,9 @@ def to_dta(stata_path, dataframe=None, output_path=None, file_name=None, force_n
         csv = output_path + file_name + '.csv'
         dta = output_path + file_name
         dataframe.to_csv(csv, sep='|', index=True, chunksize=10 ** 7)
+
+    # Stata locals
+    params = [csv, dta]
 
     # Number of columns to convert
     if force_nums is None:
@@ -51,12 +47,19 @@ def to_dta(stata_path, dataframe=None, output_path=None, file_name=None, force_n
 
     num_columns = len(force_nums)
 
-    # Stata locals
-    params = [csv, dta, str(num_columns)]
-
     # Data types
     if force_nums is not None:
+        params.extend(str(num_columns))
         params.extend(force_nums)
+
+    # Variable Labels
+    if var_labels is not None:
+        labels = [(f'label var {var}', f'{var_labels[var]}') for var in var_labels.keys()]
+        labels = list(sum(labels, ())) 
+        num_columns = len(labels)
+        params.extend(str(num_columns))
+        params.extend(labels)
+
 
     #### RUN
 
